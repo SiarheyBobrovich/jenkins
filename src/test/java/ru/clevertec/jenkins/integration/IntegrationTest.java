795bc4717a5f2016a7b38cdab1752102ff0d92d5
Sergey Bobrovich <sabobrovich@alfaleasing.ru>
2024-01-08 01:25:06 +0300

    fix IntegrationTest disabledWithoutDocker

--- src/test/java/ru/clevertec/jenkins/integration/IntegrationTest.java
+++ src/test/java/ru/clevertec/jenkins/integration/IntegrationTest.java
@@ -3,6 +3,7 @@ package ru.clevertec.jenkins.integration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -14,5 +15,6 @@ import java.lang.annotation.Target;
 @ActiveProfiles("test")
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Testcontainers(disabledWithoutDocker = true)
 public @interface IntegrationTest {
 }
