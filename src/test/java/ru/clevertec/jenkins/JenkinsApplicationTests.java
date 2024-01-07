998faa4cc2ec492adb935617b760b82454c8127c
Sergey Bobrovich <sabobrovich@alfaleasing.ru>
2024-01-07 16:39:08 +0300

    add 2 tests

--- src/test/java/ru/clevertec/jenkins/JenkinsApplicationTests.java
+++ src/test/java/ru/clevertec/jenkins/JenkinsApplicationTests.java
@@ -2,11 +2,20 @@ package ru.clevertec.jenkins;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 class JenkinsApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test1() {
+        assertThat(true)
+                .isTrue();
+    }
 
+    @Test
+    void test2() {
+        assertThat(false)
+                .isFalse();
     }
 
 }
